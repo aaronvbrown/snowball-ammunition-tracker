8e4aa31cf785921ce670423f83d81a2fc7fbc597
Aaron Brown <worldofaaron@hotmail.com>
2024-01-14 07:16:18 -0800

    Read Toboggan

---
+++ src/components/TobogganDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function TobogganDetail(props) {
+  const { toboggan, onClickingDelete } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Toboggan Detail</h1>
+      <h3>{toboggan.name} - {toboggan.origin}</h3>
+      <p><em>{toboggan.type}</em></p>
+      <button onClick={ props.onClickingEdit }>Update Toboggan</button>
+      <button onClick={()=> onClickingDelete(toboggan.id) }>Return Empty Toboggan</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+TobogganDetail.propTypes = {
+  toboggan: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default TobogganDetail;
