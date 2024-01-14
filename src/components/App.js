111f2b0f3ae52e36fae886a05823e0311a563d6a
Aaron Brown <worldofaaron@hotmail.com>
2024-01-13 20:03:27 -0800

    add SnowballList.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import SnowballControl from "./SnowballControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <SnowballControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/SnowballList.js
@@ -0,0 +1,28 @@
+import React  from "react";
+import Tobaggan from "./Tobaggan";
+import PropTypes from "prop-types";
+
+function TobogganList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.tobogganList.map((toboggan, index) =>
+        <Tobaggan
+          whenTobogganClicked = { props.onTobogganSelection }
+          name={toboggan.name}
+          origin={toboggan.origin}
+          price={toboggan.price}
+          type={toboggan.type}
+          id={toboggan.id}
+          key={index}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+TobogganList.PropTypes = {
+  tobogganList: PropTypes.array,
+  onTobogganSelection: PropTypes.func
+};
+
+export default TobogganList
